d1eb9c448d695b567b5db6fb6d9eb3533519f2b6
tom76kimo <tom76kimo@gmail.com>
2014-01-09 11:29:48 +0800

    initialize and add dotter.js

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-jQuery.Dotter
+jQuery-Dotter
 =============
 
+This is a jQuery Plugin - 'Dotter'
 Make words are not out of bounds and add '...' after the words.
---
+++ src/dotter.js
@@ -0,0 +1,55 @@
+
+;(function ($, window, document, undefined) {
+	
+	var pluginName = 'Dotter',
+		defaults = {};
+	function Dotter(elem, options){
+		this.elem = elem;
+		this.options = $.extend({}, defaults, options);
+		this._defaults = defaults;
+		this._name = pluginName;
+		
+		this.init();
+	}
+	
+	Dotter.prototype.init = function(){
+		var fontSize = this.elem.css('font-size');
+		var div = $('<span style="display: inline-block; font-size: '+fontSize+';">').appendTo('body');
+		var words = this.elem.html().toString().split('');
+		var dotLength = getDotLength(div);
+		var currentLength = [];
+		var i=0;
+		while(div.width()+(dotLength*3) <= this.elem.width() && i<words.length){
+			div.append(words[i]);
+			currentLength.push(words[i]);
+			i++;
+		}
+		if(div.width()+(dotLength*3) > this.elem.width()){
+			currentLength = currentLength.splice(0, (currentLength.length)-1);
+		}
+		var theString;
+		if(i === words.length)
+			theString = currentLength.join('');
+		else
+			theString = currentLength.join('') + '...';
+		this.elem.html(theString);
+		div.remove();
+		return this.elem;
+		function getDotLength(div){
+			div.html('.');
+			var length = div.width();
+			div.html('');
+			return length;
+		}	
+	};			
+	
+	$.fn[pluginName] = function ( options ) {
+		var self = this;
+		return this.each(function () {
+			if (!$.data(this, 'plugin_' + pluginName)) {
+				$.data(this, 'plugin_' + pluginName, new Dotter( self, options ));
+			}
+		});
+	};
+	
+})(jQuery, window, document);
